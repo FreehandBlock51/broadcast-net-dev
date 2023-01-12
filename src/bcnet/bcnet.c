f6ad65c1314a205f44e42263e210daec7237fea8
FreehandBlock51 <60456293+FreehandBlock51@users.noreply.github.com>
2023-01-12 22:49:14 +0000

    refactored sending of a packet to its own function

--- src/bcnet/bcnet.c
+++ src/bcnet/bcnet.c
@@ -69,6 +69,36 @@ int bcn_populate_packet(char *packet, size_t packetsize, bcn_key_t pkey, enum Pa
   return 0;
 }
 
+// sends a BNP packet
+int bcn_send_packet(bcn_socket_t *bcn_socket, char *packet, size_t packetsize, struct sockaddr_bcn target) {\
+  // encrypt the packet
+  char *encryptedpacket = malloc(packetsize);
+  if (!rsa_encrypt(target.key, packet, packetsize, encryptedpacket, packetsize)) {
+    free(encryptedpacket);
+    return -1; // errno should be set by rsa_encrypt
+  }
+
+  int result = 0;
+  struct sockaddr_in tempaddr = {
+    .sin_family = AF_INET,
+    .sin_addr = {
+      .s_addr = target.range.full << 16
+    },
+    .sin_port = target.port,
+    .sin_zero = {0,0,0,0,0,0,0,0}
+  };
+  for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
+    tempaddr.sin_addr.s_addr++;
+    if (sendto(bcn_socket->fd, encryptedpacket, packetsize, 0, (struct sockaddr*)&tempaddr, sizeof(struct sockaddr)) == -1 && errno != EHOSTUNREACH) { // packet can't send, but not because the host can't be reached
+      result = -1;
+      break; // cleanup is done after loop; result will be -1 & errno is set by sendto()
+    }
+  }
+
+  free(encryptedpacket);
+  return result;
+}
+
 /* Function Implementations
  * These are implementations of functions defined in bcnet.h
  */
@@ -99,42 +129,19 @@ int send_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufSize, struct
   // create the packet
   size_t packetSize = sizeof(struct bcn_packet_header) + bufSize;
   char *packet;
-  char *encryptedpacket;
-  if ((packet = malloc(packetSize)) == NULL || (encryptedpacket = malloc(packetSize)) == NULL) {
+  if ((packet = malloc(packetSize)) == NULL) {
     free(packet);
-    free(encryptedpacket); // free both in case one was allocated
     return -1; // errno set by malloc
   }
   if (!bcn_populate_packet(packet, packetSize, BCN_DEVICE_KEY.public, CONTENT, 0, bufSize / 8, buffer)) {
     free(packet);
-    free(encryptedpacket);
     return -1; // errno set by bcn_populate_packet
   }
 
-  // encrypt the packet
-  if (!rsa_encrypt(target.key, packet, packetSize, encryptedpacket, packetSize)) {
-    free(packet);
-    free(encryptedpacket);
-    return -1; // errno should be set by rsa_encrypt
-  }
-
-  // brodcast the packet
-  struct sockaddr_in tempaddr = {
-    .sin_family = AF_INET,
-    .sin_addr = {
-      .s_addr = target.range.full << 16
-    },
-    .sin_port = target.port,
-    .sin_zero = {0,0,0,0,0,0,0,0}
-  };
-  for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
-    tempaddr.sin_addr.s_addr++;
-    sendto(bcn_socket->fd, encryptedpacket, packetSize, 0, (struct sockaddr*)&tempaddr, sizeof(struct sockaddr));
-  }
+  int result = bcn_send_packet(bcn_socket, packet, packetSize, target);
 
   free(packet);
-  free(encryptedpacket);
-  return 0;
+  return result; // errno set by bcn_send_packet, if result is -1
 }
 
 int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct sockaddr_bcn* fromaddr, size_t fromaddrlen) {
@@ -152,8 +159,6 @@ int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct
   int result = 0;
 
   // recieve the packet
-  struct sockaddr_in tempaddr = (struct sockaddr_in){0};
-
   size_t packetsize = bufsize + sizeof(struct bcn_packet_header);
   char *encryptedpacket = malloc(packetsize);
   char *decryptedpacket = malloc(packetsize);
@@ -162,6 +167,8 @@ int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct
     free(decryptedpacket); // free both, in case one of the allocations worked
     return -1; // errno set by malloc
   }
+
+  struct sockaddr_in tempaddr = (struct sockaddr_in){0};
   socklen_t tempaddrlen = sizeof(tempaddr);
 
   for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
@@ -238,19 +245,7 @@ int connect_to(bcn_socket_t *bcn_socket, struct sockaddr_bcn target) {
     return -1; // errno should be set by rsa_encrypt
   }
 
-  // brodcast the packet
-  struct sockaddr_in tempaddr = {
-    .sin_family = AF_INET,
-    .sin_addr = {
-      .s_addr = target.range.full << 16
-    },
-    .sin_port = target.port,
-    .sin_zero = {0,0,0,0,0,0,0,0}
-  };
-  for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
-    tempaddr.sin_addr.s_addr++;
-    sendto(bcn_socket->fd, encryptedPacket, packetSize, 0, (struct sockaddr*)&tempaddr, sizeof(struct sockaddr));
-  }
+  bcn_send_packet(bcn_socket, encryptedPacket, packetSize, target);
 
   // cleanup
   free(packet);
